c340fe02be15d03c5b990394511a49513307d491
joemer-solid <joe_mer@hotmail.com>
2024-08-20 21:34:03 -0400

    began to add aspnetcore.identity infrastructure

--- RestaurantReviews2024.Server/Startup.cs
+++ RestaurantReviews2024.Server/Startup.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using RestaurantReviews2024.App.Services;
+using RestaurantReviews2024.Server.Data;
 
 namespace RestaurantReviews2024.Server
 {
@@ -42,11 +45,24 @@ namespace RestaurantReviews2024.Server
                 client.BaseAddress = new Uri("https://localhost:44345/");
             });
 
+            ConfigureIdentityRelatedServices(services,Configuration);
+
             // TokenProvider is a service defined in the App (RestaurantReviewsUI)
             services.AddScoped<TokenProvider>();
 
             services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
                 .AddCookie();
+        }
+
+        private static void ConfigureIdentityRelatedServices(IServiceCollection services, IConfiguration configuration)
+        {
+            
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
